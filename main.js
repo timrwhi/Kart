5d6d443b4a9f2a8557b0fdd438d27cdc509ead67
Tim White <tim@6fstudios.com>
2014-05-03 21:33:27 -0500

    made uuid better

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 jQuery(function ($) {
   var util = {
     uuid: function(){
-      //generate a unique ID
-      var i = 0;
-      return function(){
-        return i++;
-      };
+      /*jshint bitwise:false */
+      var i, random;
+      var uuid = '';
+
+      for (i = 0; i < 32; i++) {
+        random = Math.random() * 16 | 0;
+        if (i === 8 || i === 12 || i === 16 || i === 20) {
+          uuid += '-';
+        }
+        uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
+      }
+
+      return uuid;
     },
     store: function(ns, data){
       if(arguments.length > 1){
